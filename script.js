c16ebb991aa59f84a1aafc951843fba689725498
Ashley Coles <ashley@acoles.co.uk>
2024-02-09 10:32:14 +0000

    Addressing review changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const newsletter = document.querySelector('#newsletter')
+const newsletter = document.querySelector('.newsletter')
 
 newsletter.addEventListener('click', () => {
-    console.log('clicked')
+    
 })
